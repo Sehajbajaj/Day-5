94a0da02e735d1f2a0fc4587a72706b1cc32e9cb
Sehajbajaj <84325400+Sehajbajaj@users.noreply.github.com>
2021-05-22 12:24:52 +0530

    Add files via upload

---
+++ Day5-q4.py
@@ -0,0 +1,12 @@
+#Python Program to Find LCM
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+if(a>b):
+    max1=a
+else:
+    max1=b
+while(1):
+    if(max1%a==0 and max1%b==0):
+        print("LCM is:",max1)
+        break
+    max1=max1+1
